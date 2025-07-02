3dae6abe2f93af1a7857e3227f4fcefa416ec6cb
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-07-02 20:45:53 -0300

    inicia teste vetores02

---
+++ src/com/vetores/teste01/Main.java
@@ -0,0 +1,12 @@
+package com.vetores.teste01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+		Products product01 =  new Products("TV", 1500.0);
+		System.out.println(product01.getName());
+	}
+
+}
---
+++ src/com/vetores/teste01/Products.java
@@ -0,0 +1,27 @@
+package com.vetores.teste01;
+
+public class Products {
+
+	private String name;
+	private double price;
+	
+	public Products(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+
+	public double getPrice() {
+		return price;
+	}
+
+
+	
+	
+	
+	
+}
