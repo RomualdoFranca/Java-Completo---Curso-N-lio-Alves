f465346448ecd3a1ea8622e23c45bf804198515c
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-07-02 21:20:49 -0300

    implementa correcao

--- src/com/vetores/teste01/Main.java
+++ src/com/vetores/teste01/Main.java
@@ -6,23 +6,8 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Teste");
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Quantidade de produtos");
-		int quantidade = sc.nextInt();
-		
-		
-		for (int i = 0; i < args.length; i++) {
-			
-		}
-		
-		System.out.println("Nome do produto.");
-		String nome = sc.nextLine();
-		System.out.println("Preço do produto");
-		double preco = sc.nextDouble();
-		
-		Products product01 =  new Products(nome, preco);
-		System.out.println(product01.getName());
 	}
 
 }
