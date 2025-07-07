5a6d084bebea83b21dd7d1dcdb0cdbc4645c4485
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-07-07 00:54:24 -0300

    adiciona os clientes aos respectivos quartos

--- src/exercicios/vetores/pensionato/Pensionato.java
+++ src/exercicios/vetores/pensionato/Pensionato.java
@@ -10,7 +10,7 @@ public class Pensionato {
 		Scanner sc = new Scanner(System.in);
 		
 		// vetor que representa o total de quartos
-		int[] quartos = new int[10];
+		String[] quartos = new String[10];
 		
 //		int numeroQuarto = 1;
 //		for (int i = 0; i < quartos.length; i++) {
@@ -19,7 +19,7 @@ public class Pensionato {
 //		}
 //		System.out.println(quartos.length);
 		
-		System.out.println("Quantos quartos serão alugados? " );
+		System.out.println("Quantos quartos serão alugados? " );// define o tamanho do vetor dos dados dos clientes
 		int quartosAlugados = sc.nextInt();
 		sc.nextLine();
 		
@@ -27,8 +27,7 @@ public class Pensionato {
 		// vetor que armazena os dados dos clientes da pensao
 		String[] dadosClientes = new String[quartosAlugados];
 		
-		
-		
+		// adiciona os dados dos clientes no vetor
 		for (int i = 0; i < dadosClientes.length; i++) {
 			
 			System.out.println("Digite o nome:");
@@ -40,23 +39,26 @@ public class Pensionato {
 			System.out.println("Digite o número do quarto:");
 			int numeroQuarto = sc.nextInt();
 			sc.nextLine();
-			dadosClientes[i] = "Nome: " + nome + "\nEmail:" + email;
+			
+			dadosClientes[i] = numeroQuarto + ": " + nome + ", " + email;
+
+			for(int j = 0; j < quartos.length; j++) {
+				quartos[numeroQuarto] = dadosClientes[i];
+			}
+			
 		}
+		
+		
 		
 		System.out.println("Dados dos clientes:");
-		for (String string : dadosClientes) {
-			System.out.println(string);
+		for (String string : quartos) {
+			if(string != null) {
+				System.out.println(string);
+				
+			}
 		}
 		
-//		dadosClientes[quartos[0]] = "Maria";
-//		dadosClientes[quartos[1]] = "José";
-//		System.out.println("Vetor dados dos clientes:" + dadosClientes.length);
-//		
-//		for (int i = 0; i < quartos.length; i++) {
-////			if (dadosClientes[i] != null) {
-////			}
-//			System.out.println(quartos[i]);
-//		}
+		// o numero do quarto será o numero do indice do vetor dos quartos.
 		
 		sc.close();
 	}
