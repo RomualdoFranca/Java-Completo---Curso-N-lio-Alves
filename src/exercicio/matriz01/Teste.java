a4029e762dac04f5a68dfeeef4eeaf00547e6c85
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-07-09 11:17:59 -0300

    implementa metodo que define o tamanho da matriz

--- src/exercicio/matriz01/Teste.java
+++ src/exercicio/matriz01/Teste.java
@@ -7,32 +7,25 @@ public class Teste {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
-		
-		//testa a geraçao de numeros aleatorios
-//		int num = 1;
-//		int[] list = new int[10];
-//		
-//		for (int i = 0; i < list.length; i++) {
-//			list[i] = num;
-//			num++;
-//		}
-//
-//		for (int i : list) {
-//			System.out.print(i + " ");
-//		}
+
 //		System.out.println();
 		
-
-		System.out.println("Digite o numero de linhas da matriz");
-		int linha  = sc.nextInt();
-		sc.nextLine();
+//		System.out.println("Digite o numero de linhas da matriz");
+//		int linha  = sc.nextInt();
+//		sc.nextLine();
+//		
+//		System.out.println("Digite o numero de colunas da matriz");
+//		int coluna = sc.nextInt();
+//		sc.nextLine();
 		
-		System.out.println("Digite o numero de colunas da matriz");
-		int coluna = sc.nextInt();
-		sc.nextLine();
+		int linha = 0;
+		int coluna = 0;
+		int[] resultado = defineLinhaColuna(linha, coluna);
+		//verifica se os valores foram armazenados nas variaveis
+//		System.out.println("linha " + resultado[0] + "\ncoluna " + resultado[1]);
+//		
+		int[][] mat = new int[resultado[0]][resultado[1]];
 		
-		int[][] mat = new int[linha][coluna];
 		
 		System.out.println("Matriz de numeros sequenciais");
 		preencheMatrizNumerosSequenciais(mat);
@@ -42,7 +35,20 @@ public class Teste {
 		preencheMatrizNumerosAleatorios(mat);
 		imprimeValores(mat);
 		
+	}
+	
+	public static int[] defineLinhaColuna(int linha, int coluna) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite o numero de linhas da matriz");
+		linha  = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Digite o numero de colunas da matriz");
+		coluna = sc.nextInt();
+		sc.nextLine();
+		
 		sc.close();
+		return new int[] {linha, coluna};
 	}
 	
 	public static void preencheMatrizNumerosSequenciais(int[][] matriz) {
